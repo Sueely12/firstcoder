7171f7b6887b68871fa28f8eb1396ec9b5718784
suely <hellosuleymanbro@gmail.com>
2024-12-12 13:44:52 +0400

    updatee

--- FirstCoder/Assets/MenuManager.cs
+++ FirstCoder/Assets/MenuManager.cs
@@ -2,26 +2,69 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject mainMenu; // Ana menüdeki butonları tutan GameObject
-    public GameObject subMenu; // Alt menüdeki butonları tutan GameObject
+    public GameObject mainMenu;        // Ana menüdeki butonları tutan GameObject
+    public GameObject subMenu;         // Alt menüdeki butonları tutan GameObject
+    public GameObject settingsMenu;    // Ayarlar menüsünü tutan GameObject
+    public GameObject findGameMenu;    // Oyun bulma menüsünü tutan GameObject
+    public GameObject createGameMenu;  // Oyun kurma menüsünü tutan GameObject
+    public GameObject userProfileMenu; // Kullanıcı profili menüsünü tutan GameObject
 
     void Start()
     {
-        // Başlangıçta ana menü açık, alt menü kapalı
+        // Başlangıçta ana menü açık, diğer menüler kapalı
         ShowMainMenu();
     }
 
-    // Ana menüyü gösterir ve alt menüyü gizler
-    public void ShowMainMenu()
+    // Tüm menüleri kapatan yardımcı fonksiyon
+    private void HideAllMenus()
     {
-        mainMenu.SetActive(true);  // Ana menü butonlarını aktif yap
-        subMenu.SetActive(false); // Alt menü butonlarını gizle
+        mainMenu.SetActive(false);
+        subMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        findGameMenu.SetActive(false);
+        createGameMenu.SetActive(false);
+        userProfileMenu.SetActive(false);
     }
 
-    // Alt menüyü gösterir ve ana menüyü gizler
+    // Ana menüyü gösterir
+    public void ShowMainMenu()
+    {
+        HideAllMenus();
+        mainMenu.SetActive(true);
+    }
+
+    // Alt menüyü gösterir
     public void ShowSubMenu()
     {
-        mainMenu.SetActive(false); // Ana menü butonlarını gizle
-        subMenu.SetActive(true);  // Alt menü butonlarını aktif yap
+        HideAllMenus();
+        subMenu.SetActive(true);
+    }
+
+    // Ayarlar menüsünü gösterir
+    public void ShowSettingsMenu()
+    {
+        HideAllMenus();
+        settingsMenu.SetActive(true);
+    }
+
+    // Oyun bulma menüsünü gösterir
+    public void ShowFindGameMenu()
+    {
+        HideAllMenus();
+        findGameMenu.SetActive(true);
+    }
+
+    // Oyun kurma menüsünü gösterir
+    public void ShowCreateGameMenu()
+    {
+        HideAllMenus();
+        createGameMenu.SetActive(true);
+    }
+
+    // Kullanıcı profili menüsünü gösterir
+    public void ShowUserProfile()
+    {
+        HideAllMenus();
+        userProfileMenu.SetActive(true);
     }
 }
